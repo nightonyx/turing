8c8e362e1d3ec64d93f7bfbdfd2fb15cdc2cb928
aros <aros@aziron.ru>
2018-10-01 17:35:51 +0300

    Init input arguments and started checking their: -All input arguments are initialized -Checked debug flag

--- main/main.c
+++ main/main.c
@@ -1,9 +1,42 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include <stdio.h>
-#include <malloc.h>
-#include <mem.h>
 
-int main() {
+#define DEBUG_FLAG "-d"
 
-    
+#define MAIN_ARG_COUNT 5
+
+#define MAX_ARG_COUNT 6
+
+#define DEBUG_FLAG_INDEX 6
+
+int main(int argc, char *argv[]) {
+
+    const _Bool is_valid_arg_count = argc == MAIN_ARG_COUNT && argc == MAX_ARG_COUNT;
+    if (is_valid_arg_count) {
+        const char *command_file_path = argv[1];
+        const char *input_file_path = argv[2];
+        const char *output_file_path = argv[3];
+        const char *initial_state = argv[4];
+        _Bool is_debug = false;
+
+        //Check debug flag:
+        const _Bool has_debug_flag = argc == DEBUG_FLAG_INDEX;
+        if (has_debug_flag) {
+            const char *debug_flag = argv[5];
+            int is_valid_debug_flag = strncmp(debug_flag, DEBUG_FLAG, 2);
+            if (is_valid_debug_flag == 0) {
+                is_debug = true;
+            } else {
+                printf("%s\n", "Expected debug flag '[d]'");
+            }
+        }
+
+        //Define initial state
+
+
+    }
+
     return 0;
 }
